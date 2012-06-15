8f52d37e5894f2e5f665e86fc0068322f3597cb3
hemantb <hemantb@noc3.corp.inmobi.com>
2012-06-15 09:40:50 +0530

    Added files

--- cookbooks/aptFTPArchive/recipes/apt-client.rb
+++ cookbooks/aptFTPArchive/recipes/apt-client.rb
@@ -22,37 +22,6 @@ execute "curl -s http://appkg1.ev1.inmobi.com/inmobiglobal-apt.key | apt-key add
   not_if "apt-key export 'InMobi Operations'"
 end.run_action(:run)
 
-
-apt_repository "inmobi-app-apt" do
-  uri "http://#{node[:aptFTPArchive][:aptserver]}/deb/inmobi/"
-  distribution "prod"
-  components ["main","ops","3pp","3ppours"]
-#  keyserver "keyserver.ubuntu.com"
-#  key "C0061A4A"
-  action :add
-  notifies :run, "execute[apt-get update]", :immediately
-end
-
-apt_repository "pkg_ua2-apt" do
-  uri "http://pkg.ua2.inmobi.com:9999/deb/inmobi/"
-  distribution "prod"
-  components ["main","ops","3pp","3ppours"]
-#  keyserver "keyserver.ubuntu.com"
-#  key "C0061A4A"
-  action :add
-  notifies :run, "execute[apt-get update]", :immediately
-end
-
-apt_repository "pkg_ev1-apt" do
-  uri "http://pkg.ev1.inmobi.com:9999/apt/inmobi/"
-  distribution "testing"
-  components ["main"]
-#  keyserver "keyserver.ubuntu.com"
-#  key "C0061A4A"
-  action :add
-  notifies :run, "execute[apt-get update]", :immediately
-end
-
 log "Done APT key for APPOps"
 
 log " Staring apt-get update"
