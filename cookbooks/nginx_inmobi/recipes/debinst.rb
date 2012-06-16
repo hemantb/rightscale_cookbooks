824cce7c20a59af999858f92362c3d078516938d
hemantb <hemantb@noc3.corp.inmobi.com>
2012-06-17 03:05:31 +0530

    Added files

--- cookbooks/nginx_inmobi/recipes/debinst.rb
+++ cookbooks/nginx_inmobi/recipes/debinst.rb
@@ -3,7 +3,7 @@ rightscale_marker :begin
 node[:nginx_inmobi][:nginxConfPackage] .each do |p|
   log "Installing #{p}"
   case "#{p}"
-    when /(.*)[=|-](\d+.*)(\.deb|)$/
+    when /(.*?)[=|-](\d+.*)(\.deb|)$/
       package $1 do
         version $2
         action :install
