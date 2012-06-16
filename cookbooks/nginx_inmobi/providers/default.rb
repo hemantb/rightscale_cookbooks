88644fdb1a565a529574ec64cef9b729daf4b467
hemantb <hemantb@noc3.corp.inmobi.com>
2012-06-17 01:21:53 +0530

    Added files

--- cookbooks/nginx_inmobi/providers/default.rb
+++ cookbooks/nginx_inmobi/providers/default.rb
@@ -44,7 +44,7 @@ end
 action :setup_config do
   log "Creating the symlink"
   ::File.symlink("/usr/share/perl/5.10.1","/usr/share/perl/5.10")
-  not_if do ::File.symlink?("/usr/share/perl/5.10") end
+  not_if do (::File.symlink?("/usr/share/perl/5.10")) end
 #  if !File.symlink("/usr/share/perl/5.10")?
 #    File.symlink("/usr/share/perl/5.10.1","/usr/share/perl/5.10")
 #  end
