9a3d65e52337c5889e71fca7b4e58adfda0c6b80
hemantb <hemantb@noc3.corp.inmobi.com>
2012-06-17 03:01:08 +0530

    Added files

--- cookbooks/nginx_inmobi/recipes/debinst.rb
+++ cookbooks/nginx_inmobi/recipes/debinst.rb
@@ -2,20 +2,35 @@ rightscale_marker :begin
 
 node[:nginx_inmobi][:nginxConfPackage] .each do |p|
   log "Installing #{p}"
-  if (/(.*)=(.*)/.match("#{p}"))
-   # log " Installing Package" + $1 "with version" + $2
-    package $1 do
-      version $2
-      action :install
-      options "--force-yes"
+  case "#{p}"
+    when /(.*)[=|-](\d+.*)(\.deb|)$/
+      package $1 do
+        version $2
+        action :install
+        options "--force-yes"
+      end
+    else
+      package p do
+        options "--force-yes"
+        action :install
+      end
     end
-  else
-    package p do
-      options "--force-yes"
-      action :install
-    end
-  end
 end
+
+#  if (/(.*)=(.*)/.match("#{p}"))
+   # log " Installing Package" + $1 "with version" + $2
+#    package $1 do
+#      version $2
+#      action :install
+#      options "--force-yes"
+#    end
+#  else
+#    package p do
+#      options "--force-yes"
+#      action :install
+#    end
+#  end
+#end
 
 #case node[:nginx_inmobi][:restart]
 #when "true"
